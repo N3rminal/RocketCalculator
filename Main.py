e707469f3ed71c64c2432399ba80158c0b87c83c
N3rminal <88295578+N3rminal@users.noreply.github.com>
2021-08-20 23:00:52 -0500

    Fixed Names and namespace

---
+++ Main.py
@@ -0,0 +1,9 @@
+# import Required addons
+import math
+
+import Calculations
+import IO_Manager
+
+# Main Script Goes here
+Calculations.read_input('input')
+swirl_output = Calculations.simpleswirl('outputs')
